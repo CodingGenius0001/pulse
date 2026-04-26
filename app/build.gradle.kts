2a76d19c6dfc0267b1459929b72de2fe7465bf92
Keshav Pillarisetti <keshavpillarisetti@gmail.com>
2026-04-26 02:34:59 -0700

    v0.5.4: 1-line lyrics, fix wrong metadata, fix duplicates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,8 +52,8 @@ android {
         applicationId = "com.pulse.music"
         minSdk = 26
         targetSdk = 34
-        versionCode = 10
-        versionName = "0.5.3"
+        versionCode = 11
+        versionName = "0.5.4"
 
         vectorDrawables {
             useSupportLibrary = true
